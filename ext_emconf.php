07812bfec736273906495ea93ce9b134f6bf80c1
Roman Büchler <info@buechler.pro>
2021-12-14 12:31:40 +0200

    [TASK] Create extension scaffold

---
+++ ext_emconf.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the composer package buepro/typo3-fromes.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Frontend messenger',
+    'description' => 'Provides a plugin to send emails to frontend users. Recipients can be compiled by help of a flexible filter.',
+    'category' => 'plugin',
+    'author' => 'Roman Büchler',
+    'author_email' => 'rb@buechler.pro',
+    'state' => 'alpha',
+    'clearCacheOnLoad' => 0,
+    'version' => '0.0.1',
+    'constraints' => [
+        'depends' => [
+            'php'   => '>=7.3.0',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'Buepro\\Fromes\\' => 'Classes'
+        ],
+    ],
+];

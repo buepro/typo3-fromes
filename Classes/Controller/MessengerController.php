ae405c44db15302a7f007972b90b15d069865f18
Roman Büchler <info@buechler.pro>
2022-01-07 19:44:13 +0200

    [TASK] Create json data for checkbox filter

--- Classes/Controller/MessengerController.php
+++ Classes/Controller/MessengerController.php
@@ -11,6 +11,10 @@ declare(strict_types=1);
 
 namespace Buepro\Fromes\Controller;
 
+use TYPO3\CMS\Core\Localization\LanguageService;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
+
 /**
  * EventController
  */
@@ -21,5 +25,42 @@ class MessengerController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionContro
      */
     public function panelAction(): void
     {
+        $this->view->assignMultiple(['jsonFilters' => $this->getJsonFiltersFromSettings()]);
+    }
+
+    private function getJsonFiltersFromSettings(): string
+    {
+        $filters = [];
+        $languageService = GeneralUtility::makeInstance(LanguageService::class);
+        foreach($this->settings['filters'] as $id => $conf) {
+            if (($method = $this->getFilterItemsMethodName($conf['items'])) !== null) {
+                $filters[$id] = $conf;
+                $filters[$id]['title'] = $languageService->sL($filters[$id]['title']);
+                $filters[$id]['items'] = $this->$method($conf['items']);
+            }
+        }
+        return json_encode($filters);
+    }
+
+    private function getFilterItemsMethodName(array $conf): ?string
+    {
+        $methodName = 'getFilterItemsFrom' . ucfirst(strtolower($conf['source']));
+        return method_exists($this, $methodName) ? $methodName : null;
+    }
+
+    private function getFilterItemsFromTs(array $conf): array
+    {
+        $conf = $conf['ts'];
+        $cObjRenderer = new ContentObjectRenderer();
+        $items = $cObjRenderer->getRecords($conf['table'], $conf['select']);
+        return array_map(function ($item) use ($conf) {
+            $filtered = [];
+            foreach($conf['fieldMap'] as $key => $field) {
+                if (isset($item[$field])) {
+                    $filtered[$key] = $item[$field];
+                }
+            }
+            return $filtered;
+        }, $items);
     }
 }
